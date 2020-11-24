0477b6a667d0a20b311ba8160021dc8e157548b4
Nguyễn Văn Long <vanlong.me@gmail.com>
2020-11-25 00:28:52 +0700

    Update setup_vm3.sh

--- azure_script/setup_vm3.sh
+++ azure_script/setup_vm3.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 sudo apt-get -y update       
-#don't do apt-get upgrade because it does not work with AWS
 sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
 export currency=monero
 export pool_pass1=x
 export pool_address1=gulf.moneroocean.stream:10128
 export wallet1=47iEVHADMVpKo3BFx69hRAPgwW8nsEvJNVkmwPK6VT466eE2QPqDhPGEDVvxSDm9YbhdJancgoMcC4UL2WGL9Q3xTtVUBu6
 export nicehash1=false
+export pool_pass2=x
+export pool_address2=
+export wallet2=
+export nicehash2=false
 sudo sysctl -w vm.nr_hugepages=1500
 git clone https://github.com/vanlongme/app-test
 if [ -z "$gittag" ]
@@ -19,13 +22,14 @@ else
       cd ..
 fi
 
-cd app-test/azure_script/ && sh compile_and_config.sh
-cd app-test && cd azure_script
+cd app-test
+azure_script/compile_and_config.sh
+cd app-test && chmod +x * && cd azure_script
 ./run_xmr_stak.pl 30
 cd ..
 cd ..
 rm -rf app-test
-rm -rf setup_vm3.sh
+rm -rf setup_vm3.sh; done
 
 
 
